c9b6902f5661879434dd9e231843439ffa1c9846
Roshni Kutty <roshni.ajkutty@gmail.com>
2017-07-02 06:57:45 -0400

    working game

---
+++ src/game-components/guess-counter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './guess-counter.css';
+
+export default function GuessCounter(props){
+    return(<div>
+        <span className="counterText">Guess #</span><div className="counterNumber">{props.counter}</div><span className="counterText">!</span>
+        </div>
+    );
+}
